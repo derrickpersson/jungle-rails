d7fcf75c2f9c18eba77a18a9ba4d096967f8f259
Derrck Persson <derrickpersson@gmail.com>
2018-01-18 11:08:02 -0800

    Add initial user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
-class UsersControllerController < ApplicationController
+class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password_digest
+      )
   end
 end
